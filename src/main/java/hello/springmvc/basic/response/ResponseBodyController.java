cef5d923ff05d2bee432b394cd82c3c8b0fba2cd
leerobing <105533691+leerobing@users.noreply.github.com>
2023-11-03 11:32:08 +0900

    feat: 응답 API 개발을 위한 ResponseBodyController 개발

---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic.response;
+
+import hello.springmvc.basic.HelloData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ResponseBodyController {
+
+    @GetMapping("response-body-string-v4")
+    public ResponseEntity<HelloData> responseBodyControllerV4(@RequestBody HelloData helloData) {
+        HelloData data = new HelloData();
+        data.setUsername(helloData.getUsername());
+        data.setAge(helloData.getAge());
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("response-body-string-v5")
+    public HelloData responseBodyControllerV5(@RequestBody HelloData helloData) {
+        HelloData data = new HelloData();
+        data.setUsername(helloData.getUsername());
+        data.setAge(helloData.getAge());
+        return data;
+    }
+}
